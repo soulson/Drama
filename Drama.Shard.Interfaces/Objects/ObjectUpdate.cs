dbc218ca2fbaebcef517c51b21cc9081bca2e00a
soulson <rmspycha@mtu.edu>
2017-06-28 23:03:51 -0400

    started working on object-update stuff

---
+++ Drama.Shard.Interfaces/Objects/ObjectUpdate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drama.Shard.Interfaces.Objects
+{
+    public class ObjectUpdate
+    {
+    }
+}
---
+++ Drama.Shard.Interfaces/Objects/ObjectUpdateType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Drama.Shard.Interfaces.Objects
+{
+	public enum ObjectUpdateType : byte
+	{
+		Values = 0,
+		Movement = 1,
+		CreateObject = 2,
+		CreateObject2 = 3,
+		OutOfRangeObjects = 4,
+		NearObjects = 5,
+	}
+}
